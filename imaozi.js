c4f5a8918a379bf785972706550ad90d9fe5558c
rookis1 <39900829+rookis1@users.noreply.github.com>
2024-01-16 14:19:32 +0800

    Update imaozi.js

--- imaozi.js
+++ imaozi.js
@@ -1,13 +1,17 @@
+/******************************************
+ * @version 1.0.0
+ ******************************************
+###详细见同目录README
 ```Quantumult X
 [mitm]
 hostname = app.moutai519.com.cn
 
 [rewrite_local]
-https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/rookis1/ownresource/main/imaotai.js
+https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js
 
 [task_local]
 # 茅台自动预约
-0 9 * * * https://raw.githubusercontent.com/rookis1/ownresource/main/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
+0 9 * * * https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
 ```
 ******************************************/
 var $ = new Env('i茅台'),service = $.http
@@ -129,7 +133,7 @@ function Maotai() {
                 'Accept-Encoding': `gzip, deflate, br`,
                 Host: `app.moutai519.com.cn`,
                 'MT-User-Tag': `0`,
-                'MT-': ``, // 抓
+                'MT-Token': ``, // 抓
                 Connection: `keep-alive`,
                 'MT-Device-ID': ``, // 抓
                 'Accept-Language': `zh-Hans-CN;q=1`,
@@ -267,7 +271,6 @@ function Maotai() {
                 })
             }
             var { body: resp } = await service.post(options)
-            $.log(`当前resp：${resp}`)
             var { code, data, message } = JSON.parse(resp)
             if (code === 401) {
                 Message = `预约失败\ntoken失效, 请重新抓包获取`
